7bf8cd0bbf27028356f1f5b8b2a7fad38de53dd9
boualem06 <kb_hamroune@esi.dz>
2023-05-17 10:27:35 +0100

    adding the delete user  route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
-const jwt=require('jsonwebtoken') ;
-const bcrypt=require('bcryptjs') ;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 require("dotenv").config();
 
 const NewUser = async (req, res) => {
@@ -30,9 +31,37 @@ const NewUser = async (req, res) => {
     const new_user = await User.create({
         user_name,
         email,
-        password:hashedPassword,
+        password: hashedPassword,
     })
 
+    // Create a Nodemailer transporter
+    // const transporter = nodemailer.createTransport({
+    //     service: 'Gmail',
+    //     auth: {
+    //         user: 'DzXplore.web@gmail.com',
+    //         pass: 'dzxploreweb',
+    //     },
+    // });
+
+    // // Prepare the email content
+    // const mailOptions = {
+    //     from: 'DzXplore.web@gmail.com',
+    //     to: email,
+    //     subject: 'DzXplore credentials',
+    //     html:`<p>Your account in  <strong>DzXplore</strong> is created <br><br> your username is :<strong>${user_name}</strong> <br>your  email is :<strong>${email}</strong> <br>your password is :<strong>${password} </strong><br><br> <strong>Please change you password !</strong></p>`,
+      
+    // };
+
+    // // Send the email
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         console.log(error);
+    //         res.status(500).send('Error sending email');
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //         res.send('Email sent successfully');
+    //     }
+    // });
 
     res.status(201).json({
         _id: new_user.id,
@@ -42,49 +71,59 @@ const NewUser = async (req, res) => {
 }
 
 //the function that generate tokens 
-const generateToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'30d',
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
     })
 }
 
 
-const loginUser=async (req,res)=>{
-    const {email,password}=req.body ;
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
     // check for user email 
-    const user=await User.findOne({email}) ;
-    if(user && (await bcrypt.compare(password,user.password)))
-    {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
         res.json({
-            _id:user.id,
-            user_name:user.user_name,
-            email:user.email,
-            status:201,
-            token:generateToken(user.id)
+            _id: user.id,
+            user_name: user.user_name,
+            email: user.email,
+            status: 201,
+            token: generateToken(user.id)
         })
-    }else
-    {
-        res.status(400) ;
-        res.json({message:'Invalid credentials',status:400})
+    } else {
+        res.status(400);
+        res.json({ message: 'Invalid credentials', status: 400 })
     }
 }
 
 //get the actuell user
-const me=async (req,res)=>{
-    
-    const {_id,user_name,email,}=await User.findById(req.user.id) ;
+const me = async (req, res) => {
+
+    const { _id, user_name, email, } = await User.findById(req.user.id);
     res.status(200).json({
-        id:_id,
+        id: _id,
         user_name,
         email,
     })
-} 
+}
+
+
+const deleteUser=async (req,res)=>{
+    const deletedTour= await User.deleteOne({_id:req.body.id})
+    res.status(200).json(
+        deletedTour
+    )
+}
 
 
 module.exports = {
     NewUser,
     loginUser,
-    me
+    me,
+    deleteUser
 }
+
+
+
 
 
