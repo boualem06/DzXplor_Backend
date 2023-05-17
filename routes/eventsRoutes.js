e7796d7702e758b150d0aa4c3f3947495e5f125c
nabilacherifi <90573713+nabilacherifi@users.noreply.github.com>
2023-05-17 15:04:00 +0100

    Create eventsRoutes.js

---
+++ routes/eventsRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router();
+require("dotenv").config();
+const eventsController=require('../controllers/eventsController')
+const event=require('../models/event')
+const {protect}=require('../midleware/authmidleware')
+
+
+router.post("/newEvent",protect,eventsController.newEvent) ;
+router.put('/updateEvent',protect, eventsController.updateEvent);
+router.delete("/deleteEvent",protect,eventsController.deleteEvent) ;
+router.get("/get3RandomEvents",eventsController.getThreeRandomEvents) ;
+router.get("/filterEventByStatus",eventsController.filtreEventsByStatus) ; 
+router.get("/searchEventsByTitle",eventsController.searchEventsByTitle) ;
+router.get("/filterEventsByDate",eventsController.filterEventsByDate) ;
+
+module.exports=router ;
