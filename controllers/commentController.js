346670fc992bdf77c51676707a0a1e6f82abeb32
boualem06 <kb_hamroune@esi.dz>
2023-05-17 11:32:41 +0100

    adding the new comment  route

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,16 +3,12 @@ const newComment=async(req,res)=>{
   
     const {name_user ,job_user ,comment_content ,idplace }=req.body  ;
   
-
-    
-
     const comment=await Comment.create({
         name_user,
         job_user,
         comment_content,
         idplace,
     })
-    console.log(comment)
     res.status(201).json({
         _id:comment.id,
         name_user:comment.name_user ,
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router();
 require("dotenv").config();
-const userControllers=require('../controllers/commentController')
+const commentControllers=require('../controllers/commentController')
 const Comment=require('../models/comment')
 
-router.post("/newComment",userControllers.newComment) ;
+router.post("/newComment",commentControllers.newComment) ;
 
 
 module.exports=router ;
