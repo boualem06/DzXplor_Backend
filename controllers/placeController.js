8a1fc3b5e3976fd8433762ae645f3dc9bab3de15
boualem06 <kb_hamroune@esi.dz>
2023-05-17 14:35:16 +0100

    adding the route that allows to get all the places and their number of comments

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,5 +1,5 @@
 const Place = require("../models/place")
-
+const Comment=require("../models/comment")
 //adding new place 
 const newPlace = async (req, res) => {
 
@@ -34,61 +34,103 @@ const newPlace = async (req, res) => {
         city: place.city,
         theme: place.theme,
         description: place.description,
-        images : place.images ,
-        lat : place.lat ,
+        images: place.images,
+        lat: place.lat,
         long: place.long,
-        view : place.view ,
+        view: place.view,
     })
 }
-
-
 
 
 //this is the code to update a place 
 const updatePlace = async (req, res) => {
     try {
-      const result = await Place.updateOne(
-        { _id: req.body.id },
-        {
-          $set: {
-            place_title: req.body.place_title,
-            address: req.body.address,
-            state: req.body.state,
-            category: req.body.category,
-            opening_hour: req.body.opening_hour,
-            closing_hour: req.body.closing_hour,
-            transport: req.body.transport,
-            city: req.body.city,
-            theme: req.body.theme,
-            description: req.body.description,
-            images: req.body.images,
-            lat: req.body.lat,
-            long: req.body.long,
-            view: req.body.view
-          }
-        }
-      );
-  
-      res.json(result);
+        const result = await Place.updateOne(
+            { _id: req.body.id },
+            {
+                $set: {
+                    place_title: req.body.place_title,
+                    address: req.body.address,
+                    state: req.body.state,
+                    category: req.body.category,
+                    opening_hour: req.body.opening_hour,
+                    closing_hour: req.body.closing_hour,
+                    transport: req.body.transport,
+                    city: req.body.city,
+                    theme: req.body.theme,
+                    description: req.body.description,
+                    images: req.body.images,
+                    lat: req.body.lat,
+                    long: req.body.long,
+                    view: req.body.view
+                }
+            }
+        );
+
+        res.json(result);
     } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
+
 
 
 
 //get all the places 
 const getPlaces = async (req, res) => {
-    Place.find().then((data) => {
-        console.log(data);
-        res.json(data);
-    })
+    
+    
+    const getPlacesWithCommentCounts = async () => {
+      try {
+        const placesWithCommentCounts = await Place.aggregate([
+          {
+            $lookup: {
+              from: 'comments',
+              localField: '_id',
+              foreignField: 'idplace',
+              as: 'comments'
+            }
+          },
+          {
+            $project: {
+              _id: 1,
+              place_title: 1,
+              address: 1,
+              state: 1,
+              category: 1,
+              opening_hour: 1,
+              closing_hour: 1,
+              transport: 1,
+              city: 1,
+              theme: 1,
+              description: 1,
+              images: 1,
+              lat: 1,
+              long: 1,
+              view: 1,
+              commentCount: { $size: '$comments' }
+            }
+          }
+        ]);
+    
+        res.status(200).json(
+            placesWithCommentCounts
+        )
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    };
+    
+    await getPlacesWithCommentCounts();
+    
 }
 
+
 //delete place by id 
-const deletePlace=async (req,res)=>{
-    const deletedTour= await Place.deleteOne({_id:req.body.id})
+const deletePlace = async (req, res) => {
+    const deletedTour = await Place.deleteOne({ _id: req.body.id })
     res.status(200).json(
         deletedTour
     )
@@ -97,7 +139,9 @@ const deletePlace=async (req,res)=>{
 
 
 
-module.exports={
+
+
+module.exports = {
     newPlace,
     updatePlace,
     getPlaces,
