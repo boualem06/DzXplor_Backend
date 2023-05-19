81b22a6b9e6d355a353438c748ae89ed5332d620
boualem06 <kb_hamroune@esi.dz>
2023-05-19 16:10:53 +0100

    the commit after the merge

---
+++ README.md
@@ -0,0 +1,1 @@
+# DzXplor_Backend
---
+++ routes/commentRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router();
+require("dotenv").config();
+const commentControllers=require('../controllers/commentController')
+const Comment=require('../models/comment')
+const {protect}=require('../midleware/authmidleware')
+
+
+router.post("/newComment",commentControllers.newComment) ;
+router.get("/getComments",commentControllers.getComments) ;
+router.get("/getLastThreeComments",commentControllers.getLastT3Comments) ;
+router.get("/getNbComments",protect,commentControllers.getTotalCommentCount) ;
+router.get("/getCommentsByMonth/:year",protect,commentControllers.getCommentsByMonth) ;
+
+
+
+module.exports=router ;
